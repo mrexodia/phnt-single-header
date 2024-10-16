73cae0d73d9003fe4ad719b8ab7e72f64b1d93f4
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-10-17 00:58:01 +0200

    Introduce a daily check to bump systeminformer

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+pushd systeminformer
+git fetch
+git diff --exit-code HEAD origin/master -- phnt > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "[INFO] No changes to commit"
+  popd
+  exit 0
+fi
+
+NEW_HASH=$(git rev-parse --short origin/master)
+echo "[INFO] Bumping systeminformer to $NEW_HASH"
+git checkout origin/master
+popd
+
+echo "[INFO] Checking if bump-$NEW_HASH already exists"
+git ls-remote --exit-code --heads origin bump-$NEW_HASH > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "[INFO] Already bumped"
+  exit 0
+fi
+
+echo "[INFO] Creating pull request"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config user.name "github-actions[bot]"
+git checkout -b bump-$NEW_HASH
+git commit -am "Bump systeminformer to $NEW_HASH"
+git push --set-upstream origin bump-$NEW_HASH
+gh pr create --fill
